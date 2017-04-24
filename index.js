7bf2192be62130e7d2923197ef898846b889f99e
Azinum <lukaska61@gmail.com>
2017-04-24 11:09:04 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+var express = require("express");
+var app = express();
+var server = require("http").Server(app);
+var io = require("socket.io")(server);
+
+app.use(express.static(__dirname + "/static"));
+
+app.get("/", function(req, res, next) {
+    res.sendFile(__dirname + req);
+});
+
+server.listen(3000);
